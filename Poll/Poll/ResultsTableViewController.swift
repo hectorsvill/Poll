078aa298215515c1139119dd4a9be792067daacb
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-22 20:42:54 -0700

    Results tableview and class

---
+++ Poll/Poll/ResultsTableViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ResultsTableViewController.swift
+//  Poll
+//
+//  Created by Hector Steven on 4/22/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class ResultsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+	
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+	
+
+}
