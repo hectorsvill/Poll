1d260786b58d763bcf51d0ae702181fd73fa627c
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-22 20:23:08 -0700

    votingViewController

--- Poll/Poll/VotingViewController.swift
+++ Poll/Poll/VotingViewController.swift
@@ -9,16 +9,22 @@
 import UIKit
 
 class VotingViewController: UIViewController {
-	@IBOutlet weak var nameTextField: UITextField!
-	@IBOutlet weak var Month: UITextField!
-	
-	
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		
-		
-	}
-	
-	
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
