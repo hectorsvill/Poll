07ac409b2ab4bc49797ae2efeba7846c840ba560
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-22 19:08:59 -0700

    views swift files

---
+++ Poll/Poll/VotingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  VotingViewController.swift
+//  Poll
+//
+//  Created by Hector Steven on 4/22/19.
+//  Copyright © 2019 Hector Steven. All rights reserved.
+//
+
+import UIKit
+
+class VotingViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	
+}
